a093583c453e10e1d11f8d8457261f8e6328270d
trumtamky <trantrungquantk123@gmail.com>
2022-08-17 10:27:58 +0700

    fix format

---
+++ src/Services/staff.ts
@@ -0,0 +1,33 @@
+import dao from "../daos";
+import { ICreateStaff, IQueryStaff } from "../models/models";
+
+export const isEmailExist = async (email: string) => {
+    const result = await dao.Staff.findByEmail(email);
+    if (!result) {
+        return false;
+    }
+    return true;
+};
+
+export const create = async (staff: ICreateStaff) => {
+    if (await isEmailExist(staff.name)) return {data:null,msg : new Error("Staff already existed")};
+    return {data: await dao.Staff.create(staff),msg: null};
+};
+
+export const update = async (id: string, staff: ICreateStaff) => {
+    const existedStaff = await findById(id);
+    if (await isEmailExist(staff.name)) return {data:null,msg: new Error("Email of staff already existed")};
+    return {data: await dao.Staff.update(Object.assign(existedStaff,staff)),msg:null};
+};
+
+export const findById = async (id: string) => {
+    const data = await dao.Staff.findById(id);
+    if (!data) return {data:null,msg: new Error("Staff not found")};
+    return {data: data,msg:null};
+};
+
+export const getList = async (query: IQueryStaff) => {
+    query.limit = query.limit ? Math.floor(query.limit) : 20;
+    query.keyword = query.keyword ? "%" + query.keyword + "%" : "%%";
+    return {data: await dao.Staff.getList(query)};
+}
